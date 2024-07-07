53cf700e152d82f367f08b7c37347fed9e6507d0
vmorozov893 <vmorozov893@gmail.com>
2024-07-08 02:07:48 +0300

    1 задание

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет мир!");
+        var dog = 8.0;
+        System.out.println("Значение переменной dog: " + dog);
+        var cat = 3.6;
+        System.out.println("Значение переменной cat: " + cat);
+        var paper = 763789;
+        System.out.println("Значение переменной paper: " + paper);
     }
 }
